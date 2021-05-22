05e54dbf93156971245dc9e0bfbd19df7534a9fe
Cesar Eduardo Sturmer <cesar_dudutm@hotmail.com>
2021-05-22 17:27:07 -0300

    new map

--- src/components/MapBox/index.js
+++ src/components/MapBox/index.js
@@ -1,52 +1,55 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import GoogleMapReact from 'google-map-react';
+
 import styled from 'styled-components';
 
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
-import 'leaflet-defaulticon-compatibility';
-
-const ContainerMap = styled.div `
+const ContainerMap = styled.div`
   height: 60vh;
   z-index: -1;
-`
+`;
 
+function MapBox({latitude, longitude}) {
+  const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-function MapBox() {
+  const defaultProps = {
+    center: {
+      lat: -27.6192816,
+      lng: -48.5158965,
+    },
+    zoom: 11,
+  };
 
   return (
-    <ContainerMap >
-     
-      <MapContainer
-        center={[-27.6192816, -48.5158965]}
-        zoom={10}
-        scrollWheelZoom={false}
-        style={{ height: '100%', width: '100%' }}
-      >
-        <TileLayer
-        
-        
-      url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZWR1YXJkb3NjIiwiYSI6ImNrZ2F4a3h4bTBiamIycnJ5YWVpMXVrdGoifQ.b3Duv25Uxi-UxcjkjAnC0w`} />
-
-        <Marker position={[-27.6192816, -48.5158965]} draggable={true} animate={true}>
-          <Popup>Hey ! you found me</Popup>
-        </Marker>
-      </MapContainer>
-      
+    <>
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: 'AIzaSyCuuOD2A1EY-0qrAy5jpR8-kAR4utBmA0Q',
+          }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+          <AnyReactComponent
+            lat={latitude}
+            lng={longitude}
+            text="MARKER TESTE"
+          />
+        </GoogleMapReact>
+      </div>
 
       {/* <MapContainer 
-        center={[-27.6192816, -48.5158965]}
-        zoom={11}
-        style={{ width: '100%', height: '100%' }}
-      
-      >
-      <TileLayer
-                    url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+    center={[-27.6192816, -48.5158965]}
+    zoom={11}
+    style={{ width: '100%', height: '100%' }}
 
-   
+  >
+  <TileLayer
+                url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
 
-      </MapContainer> */}
-    </ContainerMap>
+
+
+  </MapContainer> */}
+    </>
   );
 }
 
